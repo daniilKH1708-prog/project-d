55f01fa73829bc29b77f8d9dff1be01d7cbe4af6
daniilKH1708-prog <daniilKH1708@icloud.com>
2026-09-11 17:23:20 +0100

    Update LOGIN.py

--- LOGIN.py
+++ LOGIN.py
@@ -4,8 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.secret_key = "my_secret_key"
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:/Users/kharc/PyCharmMiscProject/project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
